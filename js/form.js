a644f2d863794a8649468bd6ee2c3be9223278de
Шмелев Иван <ishmelev@1ditis.ru>
2019-07-05 05:02:54 +0300

    send form

--- js/form.js
+++ js/form.js
@@ -1,41 +1,50 @@
 const blockForm = document.querySelector(".block_form");
-console.log('run');
+const data = {};
 
 const uploadForm = async (url, block) => {
     const response = await fetch(url);
     if(response.ok){
         block.innerHTML = await response.text();
-        
-        const buttonForm1 = document.getElementById("button-1");
-        
-        buttonForm1.addEventListener("click", async () => {
-            const response = await fetch("/reg/form2.html", blockForm);
-            if(response.ok){
-                block.innerHTML = await response.text();
-
-                const buttonForm2 = document.getElementById("button-2");
-
-                buttonForm2.addEventListener("click", async () => {
-                    const response = await fetch("/reg/form3.html", blockForm);
-                    if(response.ok){
-                        block.innerHTML = await response.text();
-
-                        const buttonForm3 = document.getElementById("button-3");
-
-                        buttonForm3.addEventListener("click", async () => {
-                            const response = await fetch("/reg/form4.html", blockForm);
-                            if(response.ok){
-                                block.innerHTML = await response.text();
-                            }
-                        });
-                    }
-                });
-            }
-        });
     }
 }
 
-uploadForm("/reg/form1.html", blockForm);
+const getObjectData = form => {
+    const obj = {};
+    for(const input of form){
+        obj[input.name] = input.value;
+    }
+    return obj;
+}
+
+const saveData = (id) => { 
+    return new Promise(resolve => {
+
+        const form = document.querySelector('.form');
+
+        const button = document.getElementById(`button-${id}`);
+        button.addEventListener("click", (e) => {
+            Object.assign(data, getObjectData(form));
+            localStorage['data'] = JSON.stringify(data);
+            console.log(data);
+            resolve(data);
+        });
+    });
+}
+
+const queueLoad = async () => {
+    await uploadForm("/reg/form1.html", blockForm);
+    await saveData(1);
+    await uploadForm("/reg/form2.html", blockForm);
+    await saveData(2);
+    await uploadForm("/reg/form3.html", blockForm);
+    await saveData(3);
+    await uploadForm("/reg/form4.html", blockForm);
+    await saveData(4);
+}
+
+queueLoad();
+
+
 
 
 
